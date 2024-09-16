6e572c10bd1339794f2036bd3846e62ad3671f7e
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-09-16 20:54:02 +0000

    Connected DB

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,22 @@
 
 from flask import Flask, jsonify, request
 import json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{kegsouth}:{kegsouthpinecrest}@{kegsouth.mysql.pythonanywhere-services.com}/{kegsouth$default}".format(
+    username="the username from the 'Databases' tab",
+    password="the password you set on the 'Databases' tab",
+    hostname="the database host address from the 'Databases' tab",
+    databasename="the database name you chose, probably yourusername$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
 
